d9dd3e0782486931c79bc0d76a2ad4594c19499b
akshaybachhu-53 <akshaybachhu16@gmail.com>
2025-09-29 11:42:06 +0530

    Created Client And Server

---
+++ client.c
@@ -0,0 +1,16 @@
+/*
+Client Side
+socket()
+connect()
+read()
+write()
+close()
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(){
+
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,18 @@
+/*
+Server Side
+socket()
+bind()
+listen()
+accept()
+read()
+write()
+close()
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(){
+    
+    return 0;
+}
